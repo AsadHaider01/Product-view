7578b302d64283482f05aae6791ee6aedb4aaf98
AsadHaider01 <aasad.hider88@gmail.com>
2025-01-30 05:13:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+var main = document.getElementById('main');
+var one = document.getElementById('one')
+one.addEventListener("click",function(){
+main.src = 'images 1.jpeg'
+});
+
+var main = document.getElementById("main");
+var two = document.getElementById("two")
+two.addEventListener("click",function(){
+main.src = 'image 2.webp'
+});
+
+var main = document.getElementById("main");
+var three = document.getElementById("three")
+three.addEventListener("click",function(){
+main.src = 'image 3.webp'
+});
+
+var main = document.getElementById("main");
+var four = document.getElementById("four")
+four.addEventListener("click",function(){
+main.src = 'image 4.webp'
+
+
+});
+
+
+
+var main1 = document.getElementById('main1');
+var one1 = document.getElementById('one1')
+one1.addEventListener("click",function(){
+main1.src = '1.jpg'
+});
+
+var main1 = document.getElementById("main1");
+var two = document.getElementById("two2")
+two.addEventListener("click",function(){
+main1.src = '2.jpg'
+});
+
+var main1 = document.getElementById("main1");
+var three = document.getElementById("three3")
+three.addEventListener("click",function(){
+main1.src = '3.jpg'
+});
+
+var main1 = document.getElementById("main1");
+var four = document.getElementById("four4")
+four.addEventListener("click",function(){
+main1.src = '4.jpg'
+
+
+});
